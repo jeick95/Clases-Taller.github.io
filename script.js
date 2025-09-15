682150cf8d79fad9cec5cfa179f8e0a0fb1ead16
jeick95 <t01256d@ms.upla.edu.pe>
2025-09-14 19:58:49 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btnInicio').addEventListener('click', function() {
-    document.getElementById('inicio').classList.add('contenido-activo');
-    document.getElementById('trabajos').classList.remove('contenido-activo');
-});
-
-document.getElementById('btnTrabajos').addEventListener('click', function() {
-    document.getElementById('trabajos').classList.add('contenido-activo');
-    document.getElementById('inicio').classList.remove('contenido-activo');
-});
